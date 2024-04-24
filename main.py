91e428b4339ea47654cedf6808cd8ae9531a08c6
Ayana <sayana.marapoba@gmail.com>
2024-04-24 09:46:27 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# Код для aiogram
+
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+
+# Объект бота. Вставьте сюда свой токен
+bot = Bot(token="6671029563:AAEU1OjipMJrfN1n6eFJoKBSoZsYl2JxgX4")
+
+# Диспетчер
+dp = Dispatcher()
+
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Привет!")
+
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
