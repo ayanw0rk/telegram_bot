4236b5ddbd3ec65070a3c39c70df4a88b00f1765
Ayana <sayana.marapoba@gmail.com>
2024-05-06 10:27:15 +0600

    sfasd

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters.command import Command
+import random
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
@@ -13,16 +14,27 @@ bot = Bot(token="6671029563:AAEU1OjipMJrfN1n6eFJoKBSoZsYl2JxgX4")
 # Диспетчер
 dp = Dispatcher()
 
+v1 = ""
+v2 = ""
 
-# Хэндлер на команду /start
 @dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    await message.answer("Привет!")
-    await message.answer("Это телеграмм бот 'Меркурий! От создателя Аяна:)'")
+async def cmd_start(message):
+    v1 = ""
+    v2 = ""
+    await message.answer("Введите первый вариант")
+
+@dp.message(F.text)
+async def handler(message: types.Message):
+    global v1, v2    
+    if v1 == ".":                                                                    
+        v1 = message.text
+        await message.answer("Введите второй вариант")
+    elif v2 == "":
+        v2 = message.text
+        await message.answer(random.choice([v1, v2]))
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
     await dp.start_polling(bot)
-
 if __name__ == "__main__":
     asyncio.run(main())
---
+++ sad.py
@@ -0,0 +1,7 @@
+import random 
+
+
+v1 = input("1st")
+v2 = input("2nd")
+
+print(random.choice([v1, v2]))
